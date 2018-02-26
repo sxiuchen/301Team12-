c08e6399c9dea57b131f78092907a6a5adb98680
KevinHQi <klqzpro@gmail.com>
2018-02-25 20:40:51 -0700

    .

---
+++ app/src/test/java/com/example/dada/BiddedUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.dada;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kq on 2/25/2018.
+ */
+
+public class BiddedUnitTest {
+    @Test
+    public void testGetPrice(){
+
+
+    }
+}
---
+++ app/src/test/java/com/example/dada/TaskUnitTest.java
@@ -0,0 +1,13 @@
+package com.example.dada;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kq on 2/25/2018.
+ */
+
+public class TaskUnitTest {
+    @Test
+}
