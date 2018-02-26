260a6ffd982c64d2a4fbd9923e8a0d37d76bcb8b
KevinHQi <klqzpro@gmail.com>
2018-02-25 23:32:30 -0700

    update

--- app/src/androidTest/java/com/example/dada/TaskUnitTest.java
+++ app/src/androidTest/java/com/example/dada/TaskUnitTest.java
@@ -1,22 +1,17 @@
 package com.example.dada;
 
-import android.media.Image;
-import android.test.ActivityInstrumentationTestCase2;
 import android.location.Location;
+import android.media.Image;
 
 import com.example.dada.Model.Bidded;
-import com.example.dada.Model.Task;
-
-import junit.framework.TestCase;
 
 import java.util.ArrayList;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+
 /**
  * Created by rick on 25/02/2018.
  */
 
-public class TaskTest extends ActivityInstrumentationTestCase2{
+public class TaskUnitTest{
     public static final String UserName = "User1";
     public static final String Status = "Done";
     public static final String Description = "good";
@@ -28,9 +23,7 @@ public class TaskTest extends ActivityInstrumentationTestCase2{
     public static final double assigned_pri = 10.50;
     public static final float distance = 2.345f;
     public static final ArrayList<Bidded> Bidded_History = new ArrayList<Bidded>();
-    public TaskTest(){
-        super(Task.class);
-    }
+ 
 
     public void testGetName(){
 
--- app/src/test/java/com/example/dada/TaskUnitTest.java
+++ app/src/test/java/com/example/dada/TaskUnitTest.java
@@ -1,13 +1,36 @@
 package com.example.dada;
 
-import org.junit.Test;
+import android.media.Image;
+import android.test.ActivityInstrumentationTestCase2;
+import android.location.Location;
 
-import static org.junit.Assert.*;
+import com.example.dada.Model.Bidded;
+import com.example.dada.Model.Task;
 
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 /**
- * Created by kq on 2/25/2018.
+ * Created by rick on 25/02/2018.
  */
 
-public class TaskUnitTest {
+public class TaskUnitTest{
+    public static final String UserName = "User1";
+    public static final String Status = "Done";
+    public static final String Description = "good";
+    public static final Location slocation = new Location("");
+    public static final Location elocation = new Location("");
+    public static final Image picture = null;
+    public static final int requester = 0;
+    public static final int assigned_requester = 0;
+    public static final double assigned_pri = 10.50;
+    public static final float distance = 2.345f;
+    public static final ArrayList<Bidded> Bidded_History = new ArrayList<Bidded>();
 
+
+    public void testGetName(){
+
+    }
 }
