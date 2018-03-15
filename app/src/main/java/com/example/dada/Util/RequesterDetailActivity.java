d88446278ef867625af8df795d18f0ab7b9f2431
Vincentlcy <liuchuyang1997@outlook.com>
2018-03-15 00:25:43 -0600

    edited user detail

---
+++ app/src/main/java/com/example/dada/Util/RequesterDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.dada.Util;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.dada.R;
+
+public class RequesterDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_requester_detail);
+    }
+}
