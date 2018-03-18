0efda47fe2119a18ede740fb2c6852de3f34f606
florafeng <sfeng3@ualberta.ca>
2018-03-17 23:56:52 -0600

    requester and provider main page

--- app/src/main/java/com/example/dada/Model/Task/CompletedTask.java
+++ app/src/main/java/com/example/dada/Model/Task/CompletedTask.java
@@ -1,11 +1,13 @@
-//package com.example.dada.Model.Request;
-//
-///**
-// * Task that has been completed in the past.
-// * @see Task
-// */
-//public class CompletedRequest extends Task {
-//    public CompletedRequest(String riderUserName, String driverUserName, Route route, double estimatedFare) {
-//        super(riderUserName, driverUserName, route, estimatedFare);
-//    }
-//}
+package com.example.dada.Model.Task;
+
+import com.example.dada.Model.Task.Task;
+
+/**
+ * Task that has been completed in the past.
+ * @see Task
+ */
+public class CompletedTask extends Task {
+    public CompletedTask(String title, String description) {
+        super(title, description, "completed");
+    }
+}
