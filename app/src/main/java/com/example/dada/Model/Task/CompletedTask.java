f87e567bf600f4099aa939f1211d7c2b573f6e97
Aaron Wu <yuntao1@ualberta.ca>
2018-03-18 19:59:17 -0600

    task

--- app/src/main/java/com/example/dada/Model/Task/CompletedTask.java
+++ app/src/main/java/com/example/dada/Model/Task/CompletedTask.java
@@ -1,13 +1,11 @@
 package com.example.dada.Model.Task;
 
-import com.example.dada.Model.Task.Task;
-
 /**
  * Task that has been completed in the past.
  * @see Task
  */
 public class CompletedTask extends Task {
-    public CompletedTask(String title, String description) {
-        super(title, description, "completed");
+    public CompletedTask(String requesterUserName, String providerUserName, double price) {
+        super(requesterUserName, providerUserName, price);
     }
 }
