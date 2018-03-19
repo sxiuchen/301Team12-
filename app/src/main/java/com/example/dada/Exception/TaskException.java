21dec6438e4da935c90900f880d1971041fd4d73
sxiuchen <xiuchen@ualberta.ca>
2018-03-19 12:30:34 -0600

    add license

--- app/src/main/java/com/example/dada/Exception/TaskException.java
+++ app/src/main/java/com/example/dada/Exception/TaskException.java
@@ -1,3 +1,14 @@
+/* TaskException
+ *
+ * Version 1.0
+ *
+ * March 15, 2018
+ *
+ * Copyright (c) 2018 Team 12 CMPUT 301. University of Alberta - All Rights Reserved.
+ * You may use distribute or modify this code under terms and condition of the Code of Student Behaviour at University of Alberta.
+ * You can find a copy of licence in this project. Otherwise please contact contact sfeng3@ualberta.ca.
+ */
+
 package com.example.dada.Exception;
 
 /**
