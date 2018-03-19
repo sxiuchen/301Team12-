13f6ed7855a7f56262583e40e3d58da4b8df9235
KevinHQi <klqzpro@gmail.com>
2018-03-19 05:33:57 -0600

    fill in new interface

--- app/src/main/java/com/example/dada/View/RequesterAddTaskActivity.java
+++ app/src/main/java/com/example/dada/View/RequesterAddTaskActivity.java
@@ -1,11 +1,13 @@
 package com.example.dada.View;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.dada.R;
 
@@ -17,7 +19,14 @@ public class RequesterAddTaskActivity extends AppCompatActivity {
         setContentView(R.layout.activity_requester_add_task);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        
+
+        Button done_button = (Button) findViewById(R.id.newTask_done_button);
+        done_button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                finish();
+            }
+        });
+
     }
 
 }
